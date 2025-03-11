09c14e1c88384a2ecbb56a95a742a276e2718acc
Sama Khodadady <sama.khodadady@gmail.com>
2025-03-11 22:25:27 +0330

    Online-Shop Project With Reactjs & Redux-Toolkit

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -1,4 +1,3 @@
-// import { useCart } from "../context/CartContext";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import BasketCard from "../components/BasketCard";
@@ -10,24 +9,24 @@ import styles from "./CheckOutPage.module.css";
 const CheckOutPage = () => {
   const state = useSelector((store) => store.cart);
 
-  if (!state.itemsCounter) {
-    return (
-      <div className={styles.emptyCart}>
-        <img src={image} alt="empty cart" />
-        <h2>Cart is empty !!</h2>
-        <Link to="/products">Go Home Page</Link>
-      </div>
-    );
-  }
-
   return (
     <div className={styles.container}>
-      <BasketSidebar state={state} />
-      <div className={styles.products}>
-        {state.selectedItems.map((product) => (
-          <BasketCard key={product.id} data={product} />
-        ))}
-      </div>
+      {state.itemsCounter ? (
+        <>
+          <BasketSidebar state={state} />
+          <div className={styles.products}>
+            {state.selectedItems.map((product) => (
+              <BasketCard key={product.id} data={product} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className={styles.emptyCart}>
+          <img src={image} alt="empty cart" />
+          <h2>Cart is empty !!</h2>
+          <Link to="/products">Go Home Page</Link>
+        </div>
+      )}
     </div>
   );
 };
